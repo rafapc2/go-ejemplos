1150cd77fb96af0a36633bb3f54446f56b632d9a
Rafael Prudencio C <rprudencio@bancofalabella.cl>
2019-04-27 17:14:27 -0400

    primer ejemplo

---
+++ 01-Ejemplo-Basico/main.go
@@ -0,0 +1,30 @@
+ package main
+
+import "fmt"
+
+
+func add(a int,b int ) int{
+  return a +b
+}
+
+func f(mensaje string, count int ){
+  for indice := 0; indice < count; indice++ {
+    fmt.Println(mensaje, " = ", indice)
+  }
+}
+
+func main()  {
+  fmt.Println("Hello Go")
+
+  var  n1 = 50
+  var  n2 = 90
+  resultado := add(n1 , n2)
+  fmt.Println(n1 , " + " , n2 , " = ", resultado)
+
+
+  go f("paso2",n2)
+  go f("paso1",n1)
+  fmt.Scanln()
+  fmt.Println("fin")
+
+}
